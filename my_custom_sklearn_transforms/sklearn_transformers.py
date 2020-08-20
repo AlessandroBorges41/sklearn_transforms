c39252f6a8c0103c87b1bf92721f3df53fc6f5e2
Alessandro Borges <alessandroasb@gmail.com>
2020-08-20 00:18:36 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -6,38 +6,25 @@ import pandas as pd
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin)
-    def __init__(self, columns)
+class SetIndex(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
         self.columns = columns
 
-    def fit(self, X, y=None)
+    def fit(self, X, y=None):
         return self
 
-    def transform(self, X)
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
-    
-class SetIndex(BaseEstimator, TransformerMixin)
-    def __init__(self, columns)
-        self.columns = columns
-
-    def fit(self, X, y=None)
-        return self
-
-    def transform(self, X)
+    def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.set_index(self.columns, inplace=True)
     
    
-class SmoteResample(object)
-    def __init__(self)
+class SmoteResample(object):
+    def __init__(self):
         pass
 
-    def fit(self, X, y)
+    def fit(self, X, y):
         X_resampled, y_resampled = SMOTE().fit_resample(X, y)
         X_resampled = pd.DataFrame(X_resampled, columns=X.columns)
         return X_resampled, y_resampled
